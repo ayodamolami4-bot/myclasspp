3ae43810b96445d899b5a74eec1ddbd619a7f26f
Ayodamola <ayodamolami4@gmail.com>
2026-09-11 15:18:29 +0100

    age restriction

--- main.js
+++ main.js
@@ -4,3 +4,11 @@ console.log(num);
 function add(){
     console.log(num + 5);
 }
+
+let user = 20;
+
+if(user > 18){
+    console.log("User is eligible to log in.");
+}else{
+    console.log("User is not eligible to log in.");
+}
